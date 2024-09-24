c91c0494fdd17669328a16de4207695f2a5e3ce3
MargaridaMCampos <margaridacampos@Margaridas-MBP.home>
2024-09-24 14:34:37 +0100

    changing preds to news

--- run_predictions.py
+++ run_predictions.py
@@ -10,10 +10,10 @@ alpha=0.1
 #            print(dataset+'_'+loss+'_'+seed+'_'+str(alpha))
 #            avg_set_size, coverage = run_cp(dataset,loss,alpha,seed, model_type=model_type)
 
-model_type = 'vit'
-for dataset in ['CIFAR10','CIFAR100']:
+model_type = 'bert'
+for dataset in ['NewsGroups']:
     for loss in ['sparsemax','softmax','entmax']:
-        for seed in ['05','19','95','42']:
+        for seed in ['23','05','19','95','42']:
             print(dataset+'_'+loss+'_'+seed+'_'+str(alpha))
             avg_set_size, coverage = run_cp(dataset,loss,alpha,seed, model_type=model_type, epochs=5)
 
